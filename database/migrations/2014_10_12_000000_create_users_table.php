bb187fdffe5855b5d3d590e10772358555c56134
rellisapk <rllskusuma@gmail.com>
2022-03-23 18:37:40 +0700

    Multi Auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,11 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('is_Admin')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('gender')->nullable();
+            $table->date('birth')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
